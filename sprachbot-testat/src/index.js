900c76066e7175702762b8c04781f410aefe569b
Alina Holz <alina.holz@bcxp.de>
2025-06-05 15:09:00 +0200

    change dashboard access

--- sprachbot-testat/src/index.js
+++ sprachbot-testat/src/index.js
@@ -104,8 +104,9 @@ server.on('upgrade', async (req, socket, head) => {
 
 //Load user dashboard
 server.get('/', (req, res, next) => {
-  fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+  fs.readFile(path.join(__dirname, '..', 'public', 'index.html'), (err, data) => {
     if (err) {
+      console.log(err);
       res.send(500, 'Error loading dashboard');
     } else {
       res.writeHead(200, { 'Content-Type': 'text/html' });
