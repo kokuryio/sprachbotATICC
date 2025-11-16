85e44e08ae32cda0bd46502f9c9e53915ef8f0e9
Alina Holz <alina.holz@bcxp.de>
2025-11-16 11:36:10 +0100

    remove stt/tts

--- sprachbot-testat/src/bot/bot.js
+++ sprachbot-testat/src/bot/bot.js
@@ -1,16 +1,10 @@
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 const messages = require('./botMessages');
 const UserTO = require('../data/UserTO');
-const { sendVoiceReply } = require('./ttsHelper');
 const botMessages = require('./botMessages');
-const { handleIncomingAudioAttachment } = require('./sttHelper');
 const validator = require('validator');
 const countries = require("i18n-iso-countries");
 const { DateTime } = require("luxon");
-
-
-//supported audio types for user audio input
-const SUPPORTED_AUDIO_TYPES = ['audio/wav', 'audio/x-wav', 'audio/ogg'];
 
 /**
  * Contains the bot's conversational logic.
@@ -25,25 +19,7 @@ class EchoBot extends ActivityHandler {
         this.currentInput = null;
 
         this.onMessage(async (context, next) => {
-
-            //Process audio attachements
-            if (context.activity.attachments && context.activity.attachments.length > 0) {
-                const audioAttachment = context.activity.attachments.find(att =>
-                    SUPPORTED_AUDIO_TYPES.includes(att.contentType)
-                    );
-
-                try {
-                    var text = await handleIncomingAudioAttachment(audioAttachment.contentUrl);
-                    text = text.trim().replace(/\.$/, "");
-                    context.activity.text = text; //set this for fallback purposes later
-                } catch (err) {
-                    console.error(err);
-                    await sendVoiceReply(context, botMessages.voiceTranscriptionError);
-                }
-        }else{
-                text = context.activity.text;
-        }
-            
+            const text = context.activity.text || "";
 
             const result = await this.cluClient.analyzeConversation({
                 kind: "Conversation",
@@ -70,11 +46,11 @@ class EchoBot extends ActivityHandler {
             await next();
         });
 
-        //Send welcome text/voice messages
+        //Send welcome text messages
         this.onMembersAdded(async (context, next) => {
             for (const member of context.activity.membersAdded) {
                 if (member.id !== context.activity.recipient.id) {
-                    await sendVoiceReply(context, messages.welcomeMsg);
+                    await context.sendActivity(messages.welcomeMsg);
                     await this.askForInformation(context);
                 }
             }
@@ -82,13 +58,6 @@ class EchoBot extends ActivityHandler {
         });
     }
 
-    /**
-     * Processes a user message, based on the intent of the user.
-     * 
-     * @param {*} topIntent The intent of the user, recognized by the CLU model
-     * @param {*} entities The entities of the user message, containing information
-     * @param {*} context Bot context needed for responding
-     */
     async processUserMessage(topIntent, entities, context) {
         switch (topIntent) {
             case "enterInformation":
@@ -100,32 +69,20 @@ class EchoBot extends ActivityHandler {
                 break;
 
             default:
-                await sendVoiceReply(context, messages.internalError);
+                await context.sendActivity(messages.internalError);
                 this.askForInformation(context);
                 break;
         }
     }
 
-    /**
-     * Processes personal information entered by the user.
-     * 
-     * @param {*} context Bot context needed for responding
-     * @param {*} entities The entities of the user message, containing information
-     */
     async processInformation(entities, context) {
         if (this.state !== "awaitingInformation") {
-            await sendVoiceReply(context, messages.clarifyConfirmation);
+            await context.sendActivity(messages.clarifyConfirmation);
             return;
         }
         await this.verifyInput(entities, context);
     }
 
-    /**
-     * Handles confirmation from the user and continues the process.
-     * 
-     * @param {*} context Bot context needed for responding
-     * @param {*} entities The entities of the user message, containing information
-     */
     async handleConfirm(entities, context) {
         if (this.state !== "awaitingConfirmation") {
             await this.askForInformation(context);
@@ -138,21 +95,15 @@ class EchoBot extends ActivityHandler {
         if (confirmed) {
             saveUserInput(this.currentInput, this.requiredInformation[0], this.user);
             this.requiredInformation.shift();
-            await sendVoiceReply(context, messages.confirmSave);
+            await context.sendActivity(messages.confirmSave);
             await this.askForInformation(context);
         } else if (rejected) {
             await this.askForInformation(context);
         } else {
-            await sendVoiceReply(context, messages.clarifyConfirmation);
+            await context.sendActivity(messages.clarifyConfirmation);
         }
     }
 
-    /**
-     * Verifies the user input by asking for their confirmation.
-     * 
-     * @param {*} context Bot context needed for responding
-     * @param {*} entities The entities of the user message, containing information
-     */
     async verifyInput(entities, context) {
         let currentInformation = this.requiredInformation[0];
         let relevantInformation = currentInformation;
@@ -166,50 +117,35 @@ class EchoBot extends ActivityHandler {
         this.currentInput = entityValue;
         if(this.verifyInputFormat()){
             this.state = "awaitingConfirmation";
-            await sendVoiceReply(context, messages.repeatInput(entityValue, currentInformation));
-        }else{
+            await context.sendActivity(messages.repeatInput(entityValue, currentInformation));
+        } else {
            await this.clarifyInputFormat(context);
         }
     }
 
-    /**
-     * Asks the user for the next required piece of information.
-     * 
-     * @param {*} context Bot context needed for responding
-     */
     async askForInformation(context) {
         if (this.requiredInformation.length === 0) {
-            await sendVoiceReply(context, messages.endOfProcess);
+            await context.sendActivity(messages.endOfProcess);
             createUser(this.user);
             return;
         }
 
         const currentInformation = this.requiredInformation[0];
         this.state = "awaitingInformation";
-        await sendVoiceReply(context, messages.askForInformation(currentInformation));
+        await context.sendActivity(messages.askForInformation(currentInformation));
     }
 
-    /**
-     * Checks if the current given input is valid in format
-     * @returns true if input is valid
-     */
     verifyInputFormat(){
         switch(this.requiredInformation[0]){
             case "Vorname":
-                return true;
-
             case "Nachname":
+            case "Stadt":
+            case "Straße":
                 return true;
             
             case "Geburtsdatum":
                 return this.checkBirthDateFormat();
             
-            case "Stadt":
-                return true;
-
-            case "Straße":
-                return true;
-
             case "Postleitzahl":
                 return this.checkPostCode();
 
@@ -227,19 +163,14 @@ class EchoBot extends ActivityHandler {
         }
     }
 
-    /**
-     * Informs the user about the correct input format.
-     * @param context Bot context needed for responding
-     */
     async clarifyInputFormat(context){
         if(this.requiredInformation[0] === "Geburtsdatum"){
-            await sendVoiceReply(context, botMessages.clarifyBirthDate);
+            await context.sendActivity(botMessages.clarifyBirthDate);
         }else{
-            await sendVoiceReply(context, botMessages.clarifyInput(this.currentInput, this.requiredInformation[0]));
+            await context.sendActivity(botMessages.clarifyInput(this.currentInput, this.requiredInformation[0]));
         }
     }
 
-//--------------VALIDATOR FUNCTIONS------------------------------------------------//
     checkBirthDateFormat(){
         const parsedDate = parseBirthDateFromLangauageInput(this.currentInput);
         if(validator.isDate(parsedDate)){
@@ -257,9 +188,9 @@ class EchoBot extends ActivityHandler {
     checkCountry(){
         countries.registerLocale(require("i18n-iso-countries/langs/de.json"));
         const countryNamesDe = countries.getNames("de");
-          return Object.values(countryNamesDe).some(
+        return Object.values(countryNamesDe).some(
             name => name.toLowerCase() === this.currentInput.trim().toLowerCase()
-            );
+        );
     }
 
     checkMail(){
@@ -276,55 +207,20 @@ class EchoBot extends ActivityHandler {
     }
 }
 
-/**
- * Extracts a given entity value for a certain category from a given set of entities.
- * 
- * @param {*} entities The entities of the user message, containing information
- * @param {*} category The category to fetch a value for
- * @param {*} context Bot context to extract original input from if no matching entity is found
- */
 function extractEntityValue(entities, category, context) {
     const entity = entities.find(e => e.category === category);
-    let text;
-
-    if(entity === undefined){
-        text = context.activity.text;
-    }else{
-        text = entity.text;
-    }
-    return text;
+    return entity ? entity.text : context.activity.text;
 }
 
-/**
- * Parses dates from natural german language into required format
- * @param {*} inputDate date in natural language (German)
- */
 function parseBirthDateFromLangauageInput(inputDate){
     const parsedDate = DateTime.fromFormat(inputDate, "d. MMMM yyyy", { locale: "de" });
-
-    if (parsedDate.isValid) {
-        const isoDate = parsedDate.toISODate();
-        return isoDate;
-    } else {
-        console.log("Invalid date:", parsedDate.invalidExplanation);
-        return null;
-    }
+    return parsedDate.isValid ? parsedDate.toISODate() : null;
 }
 
-/**
- * Saves user input in the user object.
- * @param {*} input The user input to save
- * @param {*} category The category to save the user input in
- * 
- */
 function saveUserInput(input, category, user) {
     user[category] = input;
 }
 
-/**
- * Saves the user in the database
- * @param {*} user 
- */
 async function createUser(user){
     const dataManager = require('../data/dataManager');
     await dataManager.insertUser(user);
